e2e12773bc3e3d4119c72773a6aa8bb2606b4725
BlackStar01 <vitalwarren@yahoo.com>
2022-05-29 18:05:07 +0200

    Starting nestjs project

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
+}
+bootstrap();
