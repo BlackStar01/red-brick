454c40b6a10fc8a53f69139c01b4d53ed19d9728
BlackStar01 <vitalwarren@yahoo.com>
2022-05-29 18:41:51 +0200

    Switch to ExpresssJs

---
+++ api/src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.setGlobalPrefix('api');
+  await app.listen(3000);
+}
+bootstrap();
--- client/src/App.vue
+++ client/src/App.vue
@@ -6,7 +6,7 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello man Vue 3 app + Vi te" />
+  <HelloWorld msg="Hello man  Vue 3 app + Vite" />
 </template>
 
 <style>
